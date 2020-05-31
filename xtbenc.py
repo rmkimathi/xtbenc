2f65fe0bd89ec8abb7825db1e8201195429f0ea5
Raymond Kimathi <raymond@xtechbiz.com>
2020-05-31 17:29:38 +0300

    Update xtbenc.py

--- xtbenc.py
+++ xtbenc.py
@@ -60,7 +60,7 @@ layout = [
      sg.Button('ffprobe_out')],
     
     [sg.Frame(layout=[
-        [sg.Output(size=(130, 20))]], title='LOG')],
+        [sg.Output(size=(130, 20), font=("Consolas", 10))]], title='LOG')],
     
     [sg.Button('Convert'),
      sg.SimpleButton('Exit', button_color=('white','firebrick3'))]
